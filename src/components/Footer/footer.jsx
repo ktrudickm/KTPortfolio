16da864dc7d15ed70b37d720e946900202f092da
Kendall <ktrudickm@gmail.com>
2022-05-20 12:24:23 -0400

    Edits to footer for responsiveness on mobile. About me and link routing next

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -4,24 +4,24 @@ import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa';
 const footer = () => {
   return (
     <>
-    <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800">
-    <div class="sm:flex sm:items-center sm:justify-between">
-        <div class="flex items-center pl-8 mb-4 sm:mb-0">
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Get In Touch</span>
+    <footer class="p-4 bg-white sm:p-6 dark:bg-gray-800">
+        <div class="flex w-100 items-center justify-around">
+            <div class="flex sm:mb-0">
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Get In Touch</span>
+            </div>
+            <div class="flex space-x-6 items-center sm:justify-center sm:mt-0">
+                <a href="https://github.com/ktrudickm" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                    <FaGithub class="w-5 h-5" />
+                </a>
+                <a href="https://www.linkedin.com/in/kendall-trudick-21598a106/" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                    <FaLinkedin class="w-5 h-5"/>
+                </a>
+                <a href="mailto:ktrudickm@gmail.com" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                    <FaEnvelope class="w-5 h-5"/>
+                </a>
+            </div>
         </div>
-        <div class="flex pr-8 mt-4 space-x-6 sm:justify-center sm:mt-0">
-            <a href="https://github.com/ktrudickm" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                <FaGithub class="w-5 h-5" />
-            </a>
-            <a href="https://www.linkedin.com/in/kendall-trudick-21598a106/" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                <FaLinkedin class="w-5 h-5"/>
-            </a>
-            <a href="mailto:ktrudickm@gmail.com" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                <FaEnvelope class="w-5 h-5"/>
-            </a>
-        </div>
-    </div>
-</footer>
+    </footer>
     </>
   )
 }
